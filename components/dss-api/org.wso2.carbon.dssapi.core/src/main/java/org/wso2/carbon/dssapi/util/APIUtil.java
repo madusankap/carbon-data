f96b85d1342a0885532963b913b3c17dbf257ca1
Maduz <madusankap@wso2.com>
2014-11-24 17:25:03 +0530

    APIPublisherClient UI menu items added

--- components/dss-api/org.wso2.carbon.dssapi.core/src/main/java/org/wso2/carbon/dssapi/util/APIUtil.java
+++ components/dss-api/org.wso2.carbon.dssapi.core/src/main/java/org/wso2/carbon/dssapi/util/APIUtil.java
@@ -10,12 +10,10 @@ import org.wso2.carbon.apimgt.api.model.URITemplate;
 import org.wso2.carbon.apimgt.impl.APIConstants;
 import org.wso2.carbon.apimgt.impl.APIManagerFactory;
 import org.wso2.carbon.apimgt.impl.internal.ServiceReferenceHolder;
+
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-/**
- * Created by tharindud on 11/20/14.
- */
 public class APIUtil {
     private static final String httpPort = "mgt.transport.http.port";
     private static final String hostName = "carbon.local.ip";
@@ -37,51 +35,46 @@ public class APIUtil {
         }
         return false;
     }
+
     public void addApi(String ServiceId) {
+        if (isAPIProviderReady()) {
+            //todo check null
+            // if null --> add to map
+            APIProvider apiProvider = getAPIProvider();
+            String provider = "admin"; //todo get correct provider(username) for tenants
+            String apiVersion = "1.0.0";
+            String apiName = ServiceId;
+            String apiEndpoint = "http://" + System.getProperty(hostName) + ":" + System.getProperty(httpPort) + "/services/" + apiName;
+            String iconPath = "";
+            String documentURL = "";
+            String authType = "Any";
 
-            if (isAPIProviderReady()) {
-                //todo check null
-                // if null --> add to map
-                APIProvider apiProvider = getAPIProvider();
-
-                String provider = "admin"; //todo get correct provider(username) for tenants
-
-                String apiVersion ="1.0.0";
-
-                String apiName = ServiceId;
-
-
-                String apiEndpoint = "http://" + System.getProperty(hostName) + ":" + System.getProperty(httpPort) +"/services/"+ apiName;
-
-                String iconPath = "";
-                String documentURL = "";
-                String authType = "Any";
-
-                APIIdentifier identifier = new APIIdentifier(provider, apiName, apiVersion);
-                try {
-                    if (apiProvider.isAPIAvailable(identifier)) {
-                        //todo : do nothing ?? update API ?
-//                        apiProvider.deleteAPI(identifier);
-                        return;
-                    }
-                } catch (APIManagementException e) {
-e.printStackTrace();
+            APIIdentifier identifier = new APIIdentifier(provider, apiName, apiVersion);
+            try {
+                if (apiProvider.isAPIAvailable(identifier)) {
+                    //todo : do nothing ?? update API ?
+                    //apiProvider.deleteAPI(identifier);
+                    return;
                 }
-
-                API api = createAPIModel(apiProvider, apiName, apiEndpoint, authType, identifier);
-
-                if (api != null) {
-                    try {
-                        apiProvider.addAPI(api);
-                    } catch (APIManagementException e) {
-                    e.printStackTrace();
-                    }
-                }
-            } else {
-
+            } catch (APIManagementException e) {
+                e.printStackTrace();
             }
 
+            API api = createAPIModel(apiProvider, apiName, apiEndpoint, authType, identifier);
+
+            if (api != null) {
+                try {
+                    apiProvider.addAPI(api);
+                } catch (APIManagementException e) {
+                    e.printStackTrace();
+                }
+            }
+        } else {
+
         }
+
+    }
+
     private API createAPIModel(APIProvider apiProvider, String apiContext, String apiEndpoint, String authType, APIIdentifier identifier) {
         API api = null;
         try {
@@ -97,18 +90,19 @@ e.printStackTrace();
             api.setTransports(Constants.TRANSPORT_HTTP + "," + Constants.TRANSPORT_HTTPS);
             api.setSubscriptionAvailability(APIConstants.SUBSCRIPTION_TO_ALL_TENANTS);
             api.setResponseCache(APIConstants.DISABLED);
-            String endpointConfig = "{\"production_endpoints\":{\"url\":\" "+ apiEndpoint + "\",\"config\":null},\"endpoint_type\":\"http\"}";
+            String endpointConfig = "{\"production_endpoints\":{\"url\":\" " + apiEndpoint + "\",\"config\":null},\"endpoint_type\":\"http\"}";
             api.setEndpointConfig(endpointConfig);
 
         } catch (APIManagementException e) {
-e.printStackTrace();
+            e.printStackTrace();
         }
         return api;
     }
+
     private Set<URITemplate> getURITemplates(String endpoint, String authType) {
         //todo improve to add sub context paths for uri templates as well
         Set<URITemplate> uriTemplates = new LinkedHashSet<URITemplate>();
-        String[] httpVerbs = { "GET", "POST", "PUT", "DELETE", "OPTIONS" };
+        String[] httpVerbs = {"GET", "POST", "PUT", "DELETE", "OPTIONS"};
 
         if (authType.equals(APIConstants.AUTH_NO_AUTHENTICATION)) {
             for (int i = 0; i < 5; i++) {
@@ -136,5 +130,5 @@ e.printStackTrace();
 
         return uriTemplates;
     }
-    }
+}
 
